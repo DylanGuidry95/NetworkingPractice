5b0fcc69afe7dfc88b2d1a6c2da0d1e3dbaad896
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-01-14 09:34:23 -0600

    Networking
    Players can connect across the network
    Imported Unity Fighting Chan asset

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CharacterMovement : NetworkBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isLocalPlayer)
+            return;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        transform.Translate(x / 5,0,z / 5);
+	}
+}
