8692201166d9409d778a152c05d346b7a3d51c8c
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-01-14 14:43:36 -0600

    Gonna be salty

---
+++ Assets/Scripts/Tutorial/WebRequest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class WebRequest : MonoBehaviour
+{
+    
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
