c929664dbae3d0bcdf85b4af2189923b334042e3
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-01-15 11:44:33 -0600

    We can play videos
    All the documentation is going back to one page that doesn't help me
    what so ever.

---
+++ Assets/Scripts/Tutorial/VideoPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoPlayer : MonoBehaviour
+{
+    string url = "http://www.unity3d.com/webplayers/Movie/sample.ogg";
+
+    GUITexture gt;
+
+    IEnumerator Start()
+    {
+        WWW webstie = new WWW(url);
+
+        var movieTexture = webstie.movie;
+
+        while (!movieTexture.isReadyToPlay)
+            yield return null;
+
+        gt = GetComponent<GUITexture>();
+
+        gt.texture = movieTexture;
+
+        transform.localScale = Vector3.zero;
+        transform.position =  new Vector3(1f, -.5f, 0);
+        gt.pixelInset = new Rect(-movieTexture.width, movieTexture.width,
+            -movieTexture.height, movieTexture.height);
+
+        var aud = GetComponent<AudioSource>();
+        aud.clip = movieTexture.audioClip;
+
+        movieTexture.Play();
+        aud.Play();
+    }
+}
