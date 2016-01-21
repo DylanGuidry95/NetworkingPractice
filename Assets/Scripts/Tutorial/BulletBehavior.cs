78be30fa6fd4c69f41e8195a1dfc1a926b40262b
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-01-21 11:15:02 -0600

    Sync health please

--- Assets/Scripts/Tutorial/BulletBehavior.cs
+++ Assets/Scripts/Tutorial/BulletBehavior.cs
@@ -4,20 +4,22 @@ using System.Collections;
 public class BulletBehavior : MonoBehaviour
 {
     public float timer = 0;
-    void Awake()
-    {
-        timer = Time.deltaTime;
-    }
-
+   
 	void OnCollisionEnter(Collision a)
     {
             GameObject hit = a.gameObject;
             Combat hitCombat = hit.GetComponent<Combat>();
 
+        if(GetComponent<NetworkView>().isMine == true)
             if (hitCombat != null)
             {
                 hitCombat.TakeDmg(10);
                 Network.Destroy(this.gameObject);
             }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
     }
 }
